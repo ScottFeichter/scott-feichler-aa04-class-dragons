8394890cdde9f4d0851fb36434591cd31d04520a
Scott Feichter <scottfeichter@gmail.com>
2024-01-30 13:52:13 -0800

    dragon.js adjusted

--- classes/dragon.js
+++ classes/dragon.js
@@ -7,13 +7,23 @@ class Dragon {
     return `${this.name} breathes fire everywhere! BURN!!!`;
   }
   static getDragons(...instances) {
-    return instances;
+    let names = [];
+    for (let instance of instances) {
+      names.push(instance.name);
+    }
+    return names;
   }
 }
 
 const puff = new Dragon(`Puff`, `green`);
 console.log(puff);
 console.log(puff.breathesFire());
+
+const toothless = new Dragon("Toothless", "black");
+console.log(toothless);
+console.log(toothless.breathesFire());
+
+console.log(Dragon.getDragons(puff, toothless));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
