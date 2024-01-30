151c105104497df34464fb3c7873b16142514d96
Scott Feichter <scottfeichter@gmail.com>
2024-01-30 13:49:40 -0800

    dragon.js

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,19 @@
-// Your code here 
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!`;
+  }
+  static getDragons(...instances) {
+    return instances;
+  }
+}
+
+const puff = new Dragon(`Puff`, `green`);
+console.log(puff);
+console.log(puff.breathesFire());
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
